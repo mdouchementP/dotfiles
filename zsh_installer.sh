25bbfbc5c04ec70994361b4751d078d78315d7f7
mdouchement <marc.douchement@predicsis.com>
2016-10-01 21:08:08 +0200

    Add dotfiles to git

--- zsh_installer.sh
+++ zsh_installer.sh
@@ -40,9 +40,30 @@ else
   echo '  ~/.oh-my-zsh/themes/fragan.zsh-theme already exists'
 fi
 
+if [ ! -f ~/.gitconfig ]
+then
+  ln -s $DIR/.gitconfig ~/.gitconfig
+else
+  echo '  ~/.gitconfig already exists'
+fi
+
 if [ ! -f ~/.rspec ]
 then
   ln -s $DIR/.rspec ~/.rspec
 else
   echo '  ~/.rspec already exists'
+fi
+
+if [ ! -f ~/.gemrc ]
+then
+  ln -s $DIR/.gemrc ~/.gemrc
+else
+  echo '  ~/.gemrc already exists'
+fi
+
+if [ ! -f ~/.gorc ]
+then
+  ln -s $DIR/.gorc ~/.gorc
+else
+  echo '  ~/.gorc already exists'
 fi
